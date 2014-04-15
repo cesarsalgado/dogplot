04e561f38a4d6b0c82f27c96b10eda2f88852fd6
Cesar Salgado <cesarsvs@gmail.com>
2014-04-14 21:21:42 -0300

    First commit. Added file with method plot_hist.
    In addition to that added __init__.py so this folder becomes a
    package.
    
    Also added .gitignore to ignore .pyc files.

---
+++ plot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+def plot_hist(array, bins=None):
+  if not bins:
+    bins = array.max()
+  hist, bins = np.histogram(array, bins=bins)
+  width = 0.7 * (bins[1] - bins[0]) 
+  center = (bins[:-1] + bins[1:]) / 2 
+  plt.bar(center, hist, align='center', width=width) 
+  plt.show()
